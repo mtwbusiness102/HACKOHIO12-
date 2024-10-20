bd2f12133bb14cfe3224b4290212ad465616472c
morrill31 <149255984+morrill31@users.noreply.github.com>
2024-10-19 21:33:51 -0400

    Create slang_library.py

---
+++ slang_library.py
@@ -0,0 +1,14 @@
+library = ["skibidi", "gyatt", "rizz", "in ohio", "ohio",
+            "duke dennis", "livvy dunne", "rizzing", "rizz", "baby gronk", "sussy", "imposter", "pibby",
+             "sigma", "alpha male", "omega male", "grindset", "andrew tate", "goon cave", "goon",
+               "freddy fazbear", "colleen", "ligma","bofa" , "ballinger", "smurf cat", "blud", "dawg", "shmlawg",
+                 "ishowspeed", "turbulence", "ambatukam", "griddy",  "ocky way", "kai cenat", "fanum tax"
+                   ,"edging", "bussing" "whopper whopper", "goofy ahh", "aiden ross", "sin city", 
+                    "goated", "john pork", "grimace shake", "nathaniel b", "lightskin stare", "biggest bird",
+                      "omar", "the referee", "amogus", "uncanny", "wholesome", "reddit", "chungus" ,"keanu reeves",
+                        "pizza tower", "zesty", "poggers",  "quandale dingle", "glizzy", "ankha zone", "thug shaker", "morbin time",
+                        "morb", "dj khaled", "sisyphus", "oceangate",  "t-pose", "ugandan knuckles", "family guy", "nickeh30", "ratio", "uwu", "owo", "delulu"
+                          "opium bird", "cg5", "mewing", "fortnite", "backrooms", "gigachad", "based", "cringe", "kino", "redpilled",
+                           "bluepilled", "F", "lean", "looksmaxxing", "gassy", "social credit", "bing chilling", "xbox live", "mrbeast"
+                              ,"ice spice", "gooning", "fr", "kevin james", "josh hutcherson", "caseoh", "sugma", "kissma", "patrick bateman","discord kitten"
+                              ,"rizzler", "cap", "no cap", "hawk tuah", "frfr", "L"]
